293ff240f592cd91874ce13c2e7c6881c495fe78
Bill Jenkins <bill@jade.pyatt.lan>
2018-06-17 22:44:08 +0000

    Added Fibonacci Sequence

--- app.py
+++ app.py
@@ -10,3 +10,17 @@ print("===============================")
 today = str(date.today())
 print("Today's date is", today)
 
+# We want to print the Fibonacci Sequence
+# 0, 1, 1, 2, 3, 5, 8, etc.
+def F():
+    a,b = 0,1
+    while a <= 100:
+        yield a
+        a, b = b, a + b
+
+def print_fibonacci():
+    for cur in F():
+       print(str(cur) + ',', end='')
+
+print_fibonacci()
+print('') # Print the newline
